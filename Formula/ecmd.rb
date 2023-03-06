133dae89aad1a3aec5b9be62ef61cbde5c265ab6
miko <136057505@qq.com>
2023-03-06 21:16:13 +0800

    Update ecmd.rb for version 1.0.2

--- Formula/ecmd.rb
+++ Formula/ecmd.rb
@@ -15,7 +15,6 @@ class Ecmd < Formula
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     bin.install "ecmd"
-	system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
