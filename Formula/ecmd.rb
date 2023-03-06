4c57f34a11ec7419b802f2fe42b422798b31b9f8
miko <136057505@qq.com>
2023-03-06 17:35:00 +0800

    add6

--- Formula/ecmd.rb
+++ Formula/ecmd.rb
@@ -4,26 +4,30 @@
 class Ecmd < Formula
   desc ""
   homepage ""
-  url "https://raw.githubusercontent.com/m1koo/EasyCmd/main/packages/ecmd-1.0.tar.gz"
-  sha256 "822dbb715917ce8a259632eb8b3ef99146d87f4d90d03951ed3771041a6395f1"
+  url "https://raw.githubusercontent.com/m1koo/EasyCmd/main/packages/ecmd-1.0.1.tar.gz"
+  sha256 "fa52ce9f337b47e689bcda5501e9a1b9b9ceba2d44ff239f28ba8e7baa4b3b1d"
   license ""
 
   # depends_on "cmake" => :build
 
-def install
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     bin.install "ecmd"
-  end  
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
 
   test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ecmd-1.0`. Options passed
+    # software. Run the test with `brew test ecmd`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/ecmd", "--version"
+    system "false"
   end
 end
