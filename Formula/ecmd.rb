dfd893a50769701f8a4a5d53f9c3c99801cb0019
miko <136057505@qq.com>
2023-03-06 19:26:43 +0800

    add8

--- Formula/ecmd.rb
+++ Formula/ecmd.rb
@@ -14,7 +14,8 @@ class Ecmd < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "ecmd"
+	system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
